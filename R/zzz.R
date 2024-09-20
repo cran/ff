e9797dbc6c587553590b6ebc3b5e544f0843ad78
Jens Oehlschlägel <Jens.Oehlschlaegel@truecluster.com>
2024-09-20 15:20:03 +0000

    version 4.5.0

--- R/zzz.R
+++ R/zzz.R
@@ -17,6 +17,15 @@
 # , show_status = FALSE
 # )
 
+# > require(rhub)
+# > rhub_setup()
+# > rhub_doctor()
+# > rhub_check(platforms = c("linux", "macos", "windows", "ubuntu-clang", "ubuntu-gcc12"))
+# ✔ Found git repository at /home/jo/SIK/truecluster/ff.
+# ✔ Found GitHub PAT.                                 
+# ✔ Check started: linux, macos, windows, ubuntu-clang, ubuntu-gcc12 (aspherical-sphinx).
+# See <https://github.com/truecluster/ff/actions> for live output!
+
 
 
 .onLoad <- function(lib, pkg) {
